83aa7e64d922f4fd01029f8b0ab0dd060c73a185
Federico Serron Umpierrez <f.serron.umpierrez@gmail.com>
2021-07-09 18:48:14 -0300

    Lessons CRUD in progress

--- resources/views/instructor/courses/edit.blade.php
+++ resources/views/instructor/courses/edit.blade.php
@@ -1,44 +1,22 @@
-<x-app-layout>
+<x-instructor-layout>
 
-    <div class="container py-8 grid grid-cols-5">
+    <x-slot name="course">
+        {{ $course->slug }}
+    </x-slot>
 
-        <aside>
-            <h1 class="font-bold text-lg mb-4">Edicion del curso</h1>
-            <ul class="text-sm text-gray-600">
-                <li class="leading-7 mb-1 border-l-4 border-indigo-400 pl-2">
-                    <a href="">Informacion del curso</a>
-                </li>
-                <li class="leading-7 mb-1 border-l-4 border-transparent pl-2">
-                    <a href="">Lecciones del curso</a>
-                </li>
-                <li class="leading-7 mb-1 border-l-4 border-transparent pl-2">
-                    <a href="">Metas del curso</a>
-                </li>
-                <li class="leading-7 mb-1 border-l-4 border-transparent pl-2">
-                    <a href="">Estudiantes</a>
-                </li>
-            </ul>
-        </aside>
+    <div class="card-body text-gray-600">
+        <h1 class="text-2xl font-bold">INFORMACION DEL CURSO</h1>
+        <hr class="mt-2 mb-6">
 
-        <div class="col-span-4 card">
+        {!! Form::model($course, ['route' => ['instructor.courses.update', $course], 'method' => 'put', 'files' => true]) !!}
 
-            <div class="card-body text-gray-600">
-                <h1 class="text-2xl font-bold">INFORMACION DEL CURSO</h1>
-                <hr class="mt-2 mb-6">
+            @include('instructor.courses.partials.form')
 
-                {!! Form::model($course, ['route' => ['instructor.courses.update', $course], 'method' => 'put', 'files' => true]) !!}
-
-                    @include('instructor.courses.partials.form')
-
-                    <div class="flex justify-end">
-                        {!! Form::submit('Actualizar informacion', ['class' => 'btn btn-primary cursor-pointer']) !!}
-                    </div>
-
-                {!! Form::close() !!}
+            <div class="flex justify-end">
+                {!! Form::submit('Actualizar informacion', ['class' => 'btn btn-primary cursor-pointer']) !!}
             </div>
 
-        </div>
-
+        {!! Form::close() !!}
     </div>
     
     <x-slot name="js">
@@ -47,4 +25,4 @@
         <script src="{{ asset('js/instructor/courses/form.js') }}"></script>
     </x-slot>
 
-</x-app-layout>
+</x-instructor-layout>
